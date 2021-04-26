27c1184d679946da54c8ecd92114d6edbaccf4fb
zhongzhiguo <a513476726@qq.com>
2021-04-26 12:45:41 +0800

    #第一次提交项目代码

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <homepage></homepage>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Homepage from "@/views/Home.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    Homepage
+  }
+})
+
+export default class Home extends Vue {}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
